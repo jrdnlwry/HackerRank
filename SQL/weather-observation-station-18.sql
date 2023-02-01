ebb4c81041c1cc51e8b3f4329e8e7f9b7b13be24
jrdnlwry <tatelwry@gmail.com>
2023-02-01 18:01:32 -0500

    Create weather-observation-station-18.sql

---
+++ SQL/weather-observation-station-18.sql
@@ -0,0 +1,13 @@
+-- Manhattan distance (definition)
+
+-- Definition: The distance between two points measured along axes at right angles. In a plane with p1 at (x1, y1) and p2 at (x2, y2), it is |x1 - x2| + |y1 - y2|. 
+
+--  happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+--  happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+--  happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+--  happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+-- Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+
+
+SELECT ROUND((MIN(LAT_N) - MAX(LAT_N)) + (MIN(LONG_W) - MAX(LONG_W)),4) * -1 as manDistance
+FROM STATION
